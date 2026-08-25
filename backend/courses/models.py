2ffd5e9302de2a607cac9c9392f4e8f5f64849fd
Khaledmohamed0 <khaled.mohamed.tallat@gmail.com>
2026-08-25 17:45:24 +0300

    Prepare project for deployment

--- backend/courses/models.py
+++ backend/courses/models.py
@@ -87,32 +87,6 @@ class Lesson(models.Model):
 
 
 
-class Enrollment(models.Model):
-    student = models.ForeignKey(
-        settings.AUTH_USER_MODEL,
-        on_delete=models.CASCADE,
-        related_name="enrollments",
-    )
-
-    course = models.ForeignKey(
-        Course,
-        on_delete=models.CASCADE,
-        related_name="enrollments",
-    )
-
-    enrolled_at = models.DateTimeField(auto_now_add=True)
-
-    class Meta:
-        constraints = [
-            models.UniqueConstraint(
-                fields=["student", "course"],
-                name="unique_student_course",
-            )
-        ]
-
-    def __str__(self):
-        return f"{self.student.username} - {self.course.title}"
-
 
 class LessonProgress(models.Model):
     student = models.ForeignKey(
